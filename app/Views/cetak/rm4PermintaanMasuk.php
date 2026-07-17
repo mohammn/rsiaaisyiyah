b52aef4ede8e9f8cdcdd9c9e6bf9e480b745f11b
unknown <mvapdg@gmail.com>
2026-07-17 10:29:54 +0700

    update no rm4 menjadi RM04

--- app/Views/cetak/rm4PermintaanMasuk.php
+++ app/Views/cetak/rm4PermintaanMasuk.php
@@ -158,7 +158,7 @@ if ($data->rm4PermintaanMasuk) {
                     </div>
                     <div class="col-5">
                         <div style="text-align: end;">
-                            RM 4
+                            RM 04
                         </div>
                         <div class="border border-dark" style="display: flex; justify-content: center;">
                             <table class="table table-borderless table-sm  mt-1 mb-1 tabel" style="font-size: xx-small;">
