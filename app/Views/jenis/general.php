ea9e3043d1f01b6fc99ae01deb5c7da813110d5d
unknown <mvapdg@gmail.com>
2026-09-04 00:14:38 +0700

    fix bug cek pers rajal

--- app/Views/jenis/general.php
+++ app/Views/jenis/general.php
@@ -52,11 +52,11 @@
                                 <?= $data->status["persRajal"][0] ?>
                             </span>
                         </td>
-                        <td><?= !empty($data->persRajal['ttdWali'] and $data->persRajal['ttdSaksi']) ? '<span class="badge-estetik bg-vibrant-teal">Sudah</span>' : '<span class="badge-estetik bg-vibrant-red">Belum</span>' ?></td>
+                        <td><?= !empty($data->persRajal['ttdWali']) && !empty($data->persRajal['ttdSaksi']) ? '<span class="badge-estetik bg-vibrant-teal">Sudah</span>' : '<span class="badge-estetik bg-vibrant-red">Belum</span>' ?></td>
                         <td>
                             <?php if ($data->persRajal) : ?>
                                 <a href="<?= base_url(" rm/persetujuanRajal/" . str_replace('/', '-', $data->pasien["no_rawat"])) ?>" style="text-decoration: none;" class="btn-estetik btn-sm-estetik bg-vibrant-purple"><i class="fas fa-search"></i> Lihat</a>
-                                <?= !empty($data->persRajal['ttdWali'] and $data->persRajal['ttdSaksi']) ? '<a href="' . base_url('/rm/persetujuanRajal/cetak/' . str_replace('/', '-', $data->pasien['no_rawat'])) . '" target="_blank" style="text-decoration: none;" class="btn-estetik btn-sm-estetik bg-vibrant-teal"><i class="fas fa-print"></i> Cetak</a>' : '<a href="' . base_url('/rm/persetujuanRajal/cetak/' . str_replace('/', '-', $data->pasien['no_rawat'])) . '" target="_blank" style="text-decoration: none;" class="btn-estetik btn-sm-estetik bg-vibrant-blue"><i class="fas fa-pen-nib"></i> TTD</a>' ?>
+                                <?= !empty($data->persRajal['ttdWali']) && !empty($data->persRajal['ttdSaksi']) ? '<a href="' . base_url('/rm/persetujuanRajal/cetak/' . str_replace('/', '-', $data->pasien['no_rawat'])) . '" target="_blank" style="text-decoration: none;" class="btn-estetik btn-sm-estetik bg-vibrant-teal"><i class="fas fa-print"></i> Cetak</a>' : '<a href="' . base_url('/rm/persetujuanRajal/cetak/' . str_replace('/', '-', $data->pasien['no_rawat'])) . '" target="_blank" style="text-decoration: none;" class="btn-estetik btn-sm-estetik bg-vibrant-blue"><i class="fas fa-pen-nib"></i> TTD</a>' ?>
                                 <a href="<?= base_url('/rm/persetujuanRajal/' . str_replace('/', '-', $data->pasien['no_rawat']))  ?>#modalHapus" style="text-decoration: none;" class="btn-estetik btn-sm-estetik bg-vibrant-red"><i class="fas fa-trash"></i> Hapus</a>
                             <?php else: ?>
                                 <a href="<?= base_url(" rm/persetujuanRajal/" . str_replace('/', '-', $data->pasien["no_rawat"]))  ?>" class="btn-estetik btn-sm-estetik bg-vibrant-blue" style="text-decoration: none;"><i class="fas fa-plus"></i> Tambah</a>
